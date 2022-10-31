35804d88e5bd885efa3bb7180b229ad294f2f7c6
daryl-goh <goh.daryl@gmail.com>
2022-10-31 11:51:13 +0800

    Number of Good Pairs Solution

---
+++ array/numberofgoodpairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        j = 0
+        count = 0
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+            
+        return count
+        
