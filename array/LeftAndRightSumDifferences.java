aa19b2ae1d0197babf37fca0f9a7934ba7c46832
daryl-goh <goh.daryl@gmail.com>
2023-03-10 12:26:35 +0800

    Left and Right Sum Differences Java Solution

---
+++ array/LeftAndRightSumDifferences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] leftRigthDifference(int[] nums) {
+
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int num: nums) {
+            rightSum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            rightSum -= val;
+            nums[i] = Math.abs(leftSum - rightSum);
+            leftSum += val;
+        }
+        return nums;
+    }
+}
