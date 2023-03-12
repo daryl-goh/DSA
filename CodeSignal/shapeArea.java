22c3ea0f5b30847a1dad97161d146e116ce85b4b
daryl-goh <goh.daryl@gmail.com>
2023-03-12 17:29:22 +0800

    Code Signal shapeArea Solution

---
+++ CodeSignal/shapeArea.java
@@ -0,0 +1,13 @@
+package leetcode.CodeSignal;
+
+public class shapeArea {
+    int solution(int n) {
+        // n = 1 : 1 
+        // n = 2 : 5  
+        // n = 3 : 13 
+        // n = 4 : 25 
+        // n = 5 : 41 
+        
+        return n * ((n - 1) * 2) + 1;
+    }
+}
