ef9f89560671e814086260d39d6775e0cb7b6526
daryl-goh <goh.daryl@gmail.com>
2023-03-12 17:28:29 +0800

    Code Signal checkPalindrome Solution

---
+++ CodeSignal/checkPalindrome.java
@@ -0,0 +1,14 @@
+package leetcode.CodeSignal;
+
+public class checkPalindrome {
+
+    boolean solution(String inputString) {
+        // Replace all that are not alphanumeric to "" and convert to LowerCase
+        String stringFilter = inputString.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        // Reverse the stringFilter
+        String reverse = new StringBuffer(stringFilter).reverse().toString();
+        // If they are equal, return true
+        return stringFilter.equals(reverse);
+    }
+    
+}
