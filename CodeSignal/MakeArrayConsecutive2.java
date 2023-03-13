8b7a80b81cfd8a3fd01d98fc61424b8c7ea3d5e9
daryl-goh <goh.daryl@gmail.com>
2023-03-13 20:44:15 +0800

    Make Array Consecutive 2 Solution for Code Signal

---
+++ CodeSignal/MakeArrayConsecutive2.java
@@ -0,0 +1,19 @@
+package leetcode.CodeSignal;
+
+public class MakeArrayConsecutive2 {
+
+    int solution(int[] statues) {
+        int count = 0;
+        Arrays.sort(statues);
+        System.out.println(statues);
+        for (int i = 0 ; i < statues.length - 1; i++) {
+            if (statues[i+1] - statues[i] > 1) {
+                count += (statues[i+1] - statues[i] - 1);
+            }
+        }
+        return count;
+    
+    }
+    
+    
+}
