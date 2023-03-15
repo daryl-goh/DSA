708297b0e762013bbd5f2316b83a6d67aecee39e
daryl-goh <goh.daryl@gmail.com>
2023-03-15 17:23:35 +0800

    Renamed 3Sum file, added comments to 3Sum Java Solutions for Leetcode

--- array/ThreeJava.java
+++ array/ThreeJava.java
@@ -1,46 +1,57 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-       List<List<Integer>> numList = new ArrayList<>();
-       
-       if (nums == null || nums.length < 3) {
-           return numList;
-       }
 
-       // Sort the array
-       Arrays.sort(nums);
+        List<List<Integer>> numList = new ArrayList<>();
 
-       for (int i = 0; i < nums.length - 2; i++) {
-           // Skip duplicate elements
-           if (i > 0 && nums[i] == nums[i-1]) {
-               continue;
-           }
+        // Set the constraints: if nothing in nums or length of nums < 3, return empty
+        // list
+        if (nums == null || nums.length < 3) {
+            return numList;
+        }
 
-           int left = i + 1;
-           int right = nums.length - 1;
-           
-           while (left<right) {
-               int sum = nums[i] + nums[left] + nums[right];
-               if (sum == 0) {
-               numList.add(Arrays.asList(nums[i], nums[left], nums[right]));
-               left++;
-               right--;
+        // Sort the array
+        Arrays.sort(nums);
 
-               // Skip duplicate elements
-               while(left < right && nums[left] == nums[left-1]) {
-                   left++;
-               }
-               // Skip duplicate elements
-               while(left < right && nums[right] == nums[right+1]) {
-                   right--;
-               }
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-           }
-       }
-       return numList;
+
+            // Set the left and right pointers
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                // Check if the sum of the three elements is 0
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // If sum == 0, add the elements to the list
+                if (sum == 0) {
+                    numList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Increment left and decrement right pointers
+                    left++;
+                    right--;
+
+                    // After incremtn, if the left element is the same as the previous element, increment again
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    // After decrement, if the right element is the same as the previous element, decrement again
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                // If the sum is less than 0, increment left pointer (array is sorted so this will increase the sum)
+                } else if (sum < 0) {
+                    left++;
+                // If the sum is greater than 0, decrement right pointer (array is sorted so this will decrease the sum)
+                } else {
+                    right--;
+                }
+            }
+        }
+        // Return the list of lists
+        return numList;
     }
 }
