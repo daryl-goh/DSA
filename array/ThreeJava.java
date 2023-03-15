4dfb85e8dc8bf99c20ed5ffae0ef3f0c28f3fcbf
daryl-goh <goh.daryl@gmail.com>
2023-03-15 17:08:28 +0800

    3Sum Java Solution for Leetcode

---
+++ array/ThreeJava.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+       List<List<Integer>> numList = new ArrayList<>();
+       
+       if (nums == null || nums.length < 3) {
+           return numList;
+       }
+
+       // Sort the array
+       Arrays.sort(nums);
+
+       for (int i = 0; i < nums.length - 2; i++) {
+           // Skip duplicate elements
+           if (i > 0 && nums[i] == nums[i-1]) {
+               continue;
+           }
+
+           int left = i + 1;
+           int right = nums.length - 1;
+           
+           while (left<right) {
+               int sum = nums[i] + nums[left] + nums[right];
+               if (sum == 0) {
+               numList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+               left++;
+               right--;
+
+               // Skip duplicate elements
+               while(left < right && nums[left] == nums[left-1]) {
+                   left++;
+               }
+               // Skip duplicate elements
+               while(left < right && nums[right] == nums[right+1]) {
+                   right--;
+               }
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+           }
+       }
+       return numList;
+    }
+}
