47d9bc9fd8f63993ff64d14df6d1062236d0078e
daryl-goh <goh.daryl@gmail.com>
2023-04-07 14:11:48 +0800

    CodeSignal Practice Questions Solutions Edit

--- CodeSignal/SingleFunction5b.java
+++ CodeSignal/SingleFunction5b.java
@@ -3,6 +3,16 @@
  * where a[i] ∘ a[j] is the concatenation of the string representations of a[i] and a[j] respectively.
  */
 
+ /* Tests passed: 16/16. 
+    Sample tests:
+    8/8
+    Hidden tests:
+    8/8
+    Score:
+    500/500
+*/
+
+
  long solution(int[] a) {
     long sum1 = 0;
     long sum2 = 0;
@@ -18,3 +28,43 @@
     
     return sum;
 }
+
+
+
+
+/*
+    Tests passed: 14/16. 
+    Execution time limit exceeded on test 15: Program exceeded the execution time limit. Make sure that it completes execution in a few seconds for any possible input.
+    Click the "Run Tests" button to see output, console logs, and detailed error messages for sample or custom test cases. 
+    This information is hidden when clicking the "Submit" button in order to prevent hard-coding solutions to the hidden test cases.
+    Sample tests:
+    8/8
+    Hidden tests:
+    6/8
+    Score:
+    417/500
+ */
+
+
+
+
+long solution(int[] a) {
+    long sum = 0;
+   StringBuilder sb = new StringBuilder();
+   
+   for (int i = 0; i < a.length; i++) {
+       for (int j = 0; j < a.length; j++) {
+           // if (i == j) {
+           //     continue;
+           // }
+           sb.append(a[i]).append(a[j]);
+           sum += Long.parseLong(sb.toString());
+
+           // sets the length of the internal character array to zero, which effectively removes all characters from the StringBuilder.
+           sb.setLength(0);
+       }
+   }
+   return sum;
+}
+
+sysout  
