e6ebe73f6bae0398ae6215bc301a5db58a14fb20
daryl-goh <goh.daryl@gmail.com>
2023-08-09 14:33:29 +0800

    SingleFunction8b Python Solution - Code Signal

--- CodeSignal/SingleFunction8b.java
+++ CodeSignal/SingleFunction8b.java
@@ -3,13 +3,18 @@ int solution(int[] a, int k) {
     /*
     When we are shifting the pointers in this binary search implementation, we are shifting them based on the value of the element, not the position.
     
-    Here's why: we are searching for a specific value that represents the length of the ribbon that can be cut into k pieces. This value could be anywhere between 1 and the maximum length of the ribbons in the input array.
+    Here's why: we are searching for a specific value that represents the length of the ribbon that can be cut into k pieces. 
+    This value could be anywhere between 1 and the maximum length of the ribbons in the input array.
     
-    Initially, we set the left pointer to 0 and the right pointer to the maximum value of the ribbons in the input array. We then use binary search to narrow down the range of possible values by computing the sum of the number of pieces that can be cut from each ribbon of length mid, where mid is the midpoint between the left and right pointers.
+    Initially, we set the left pointer to 0 and the right pointer to the maximum value of the ribbons in the input array. 
+    We then use binary search to narrow down the range of possible values by computing the sum of the number of pieces that can be cut from each ribbon of length mid, 
+    where mid is the midpoint between the left and right pointers.
     
-    If the sum of the number of pieces is greater than or equal to k, then we know that mid could be a valid answer, so we move the left pointer to mid. Otherwise, we know that mid is too large, so we move the right pointer to mid - 1.
+    If the sum of the number of pieces is greater than or equal to k, then we know that mid could be a valid answer, 
+    so we move the left pointer to mid. Otherwise, we know that mid is too large, so we move the right pointer to mid - 1.
     
-    By shifting the pointers based on the value of the element, we can efficiently search for the maximum possible length of the ribbon that can be cut into k pieces without having to check each value one by one.
+    By shifting the pointers based on the value of the element, 
+    we can efficiently search for the maximum possible length of the ribbon that can be cut into k pieces without having to check each value one by one.
     */
     
             int left = 0;
@@ -39,8 +44,10 @@ int solution(int[] a, int k) {
     }
     
     /*
-    When we exit the while loop, the value of the left pointer represents the maximum possible length of the ribbon. If left is not 0, it means that we have found a valid solution, and we return left. Otherwise, it means that there is no valid solution, and we return -1 to indicate that.
+    When we exit the while loop, the value of the left pointer represents the maximum possible length of the ribbon. 
+    If left is not 0, it means that we have found a valid solution, and we return left. Otherwise, it means that there is no valid solution, and we return -1 to indicate that.
     
-    We do not return the sum because the sum only represents the number of pieces we can obtain by cutting the ribbons into pieces of length "left." It does not give us the actual length of the ribbon that can be cut into k pieces.
+    We do not return the sum because the sum only represents the number of pieces we can obtain by cutting the ribbons into pieces of length "left." 
+    It does not give us the actual length of the ribbon that can be cut into k pieces.
     */
     
---
+++ CodeSignal/SingleFunction8b.py
@@ -0,0 +1,28 @@
+def solution(a, k):
+    # Initialize the left and right pointers for binary search
+    left, right = 0, max(a)
+    
+    while(left < right):
+        # Calculate the mid point for the current binary search iteration
+        mid = (left + right + 1) // 2
+        
+        # Initialize the sum of ribbons for this iteration
+        total_ribbons = 0
+        
+        # Calculate the total number of ribbons that can be cut with the current mid length
+        for ribbon_length in a:
+            total_ribbons += ribbon_length // mid
+        
+        # Compare the total ribbons with the required number of ribbons (k)
+        if total_ribbons >= k:
+            # If the total ribbons are greater than or equal to k, update the left pointer
+            left = mid
+        else:
+            # If the total ribbons are less than k, update the right pointer
+            right = mid - 1
+    
+    # Check if there's a valid ribbon length found
+    if left != 0:
+        return left  # Return the maximum ribbon length to obtain at least k ribbons
+        
+    return -1  # If no valid ribbon length is found
