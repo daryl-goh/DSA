f4103db45f3085d001267e8910c0c4a36f4196b8
daryl-goh <goh.daryl@gmail.com>
2023-08-21 13:26:55 +0800

    Reverse Linked List Updated Comments Python Solution - Leetcode

--- LeetCode/linkedlist/reverselinkedlist.py
+++ LeetCode/linkedlist/reverselinkedlist.py
@@ -5,15 +5,19 @@
 #         self.next = next
 class Solution(object):
     def reverseList(self, head):
+        # Iterative process using 2 pointers
         # Initialize prev pointer as null
         prev = None
         # Initialize curr pointer as the head
         curr = head
         # while curr is not null (Run a loop till curr points to NULL)
         while curr:
-            # Initialize next pointer as next pointer of curr
+            # Initialize a temporary variable nxt to store curr's next pointer
+            # This is done because we are going to change curr's next pointer
+            # and we will lose the next node so we store it in nxt
             nxt = curr.next
             # Assign the prev pointer to curr's next pointer (REVERSE IT)
+            # This is done because we are reversing the linked list
             curr.next = prev
             # Move prev to curr
             prev = curr
